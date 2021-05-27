2854074e6620f16e5c3a4e6f72130540a195ad08
drichmond3 <marisa@riskgenius.com>
2021-05-26 21:19:48 -0500

    add completedPlots to status output

--- chia-auto-plotter.js
+++ chia-auto-plotter.js
@@ -90,6 +90,7 @@ let printStatus = async ()=> {
     let timeReaminingInMilliseconds = completedPhases ? timeSpentInMilliseconds* remainingPhases / completedPhases : 0;
     response[drive] = {
       totalPlots: drivePlottingData.plotCount,
+      completedPlots: completedPhases/4,
       timeSpentInHours: timeSpentInMilliseconds/1000/60/60,
       timeRemaining: timeReaminingInMilliseconds/1000/60/60,
     }
